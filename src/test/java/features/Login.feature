a04a59b7f7535dd1a6abfbac8202d27857577337
Artur Torres <artur.sukiennik@wipro.com>
2019-10-02 12:29:11 +0100

    [1] Runner class added to run the test. [2] Multiple Test Cases applied. [3] Used regular expressions to not repeat scenarios in the step definition class.

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -2,6 +2,12 @@ Feature: Login Testing Application
 
 Scenario: Home Page Login
 Given User is on the Home Page
-When User logs in with a username and password
+When User logs in with a username "artur" and passwrod "12345"
 Then User accesses their profile page
 And User can access his personal details
+
+Scenario: Home Page Login
+Given User is on the Home Page
+When User logs in with a username "henry" and passwrod "pass"
+Then User accesses their profile page
+And User cannot access all personal details
--- src/test/java/stepdefinition/StepDefinition.java
+++ src/test/java/stepdefinition/StepDefinition.java
@@ -1,28 +1,36 @@
 package stepdefinition;
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 public class StepDefinition {
-	@Given("^User is on the Home Page$")
-	public void userIsOnHomePage() {
-		
-	}
-	
-	@When("^User logs in with a username and password$")
-	public void userLogsWithUsernameAndPassword() {
-		
-	}
-	
-	@Then("^User accesses their profile page$")
-	public void userAccessProfilePage() {
-		
-	}
-	
-	@And("^User can access his personal details$")
-	public void userAccessPersonalDetails() {
-		
-	}
+    @Given("^User is on the Home Page$")
+    public void user_is_on_the_home_page() throws Throwable {
+    	System.out.println("User has navigated to the Home Page!");
+    }
+    
+    @When("^User logs in with a username \"([^\"]*)\" and passwrod \"([^\"]*)\"$")
+    public void user_logs_in_with_a_username_and_passwrod(String arg1, String arg2) throws Throwable {
+    	System.out.println("User logged in with a Username and Password!");
+    }
+
+    @Then("^User accesses their profile page$")
+    public void user_accesses_their_profile_page() throws Throwable {
+    	System.out.println("User accessed their Profile Page!");
+    }
+
+    @And("^User can access his personal details$")
+    public void user_can_access_his_personal_details() throws Throwable {
+    	System.out.println("User can access their Personal Details!");
+    }
+    
+    @Then("^User cannot access all personal details$")
+    public void user_cannot_access_all_personal_details() throws Throwable {
+    	System.out.println("User cannot access all Personal Details!");
+    }
 }
