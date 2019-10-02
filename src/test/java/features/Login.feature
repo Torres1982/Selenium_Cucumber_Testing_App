8b44739cfc46f033b8699bdc46c42e486fd9b2ed
Artur Torres <artur.sukiennik@wipro.com>
2019-10-02 11:26:38 +0100

    [1] New feature file created - adding a Test Case for Login. [2] Step DEfinition file created - methods that perform operations depending on the conditions.

---
+++ src/test/java/features/Login.feature
@@ -0,0 +1,7 @@
+Feature: Login Testing Application
+
+Scenario: Home Page Login
+Given User is on the Home Page
+When User logs in with a username and password
+Then User accesses their profile page
+And User can access his personal details
---
+++ src/test/java/stepdefinition/StepDefinition.java
@@ -0,0 +1,28 @@
+package stepdefinition;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinition {
+	@Given("^User is on the Home Page$")
+	public void userIsOnHomePage() {
+		
+	}
+	
+	@When("^User logs in with a username and password$")
+	public void userLogsWithUsernameAndPassword() {
+		
+	}
+	
+	@Then("^User accesses their profile page$")
+	public void userAccessProfilePage() {
+		
+	}
+	
+	@And("^User can access his personal details$")
+	public void userAccessPersonalDetails() {
+		
+	}
+}
