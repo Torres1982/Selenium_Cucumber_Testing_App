62f40c6e204712d8fd738e2dbc568cca5f3b3662
Artur Torres <artur.sukiennik@wipro.com>
2019-10-02 10:16:25 +0100

    First commit

---
+++ src/main/java/torres/com/cucumber/App.java
@@ -0,0 +1,13 @@
+package torres.com.cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
