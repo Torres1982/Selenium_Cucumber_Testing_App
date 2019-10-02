f34b201dd4a2682b82cdfeed23e08099d187bc7e
Artur Torres <artur.sukiennik@wipro.com>
2019-10-02 15:15:58 +0100

    [1] Data Table added for multiple parameters Test Case. [2] Usage of Parameterrisation for multiple input parameters.

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -4,10 +4,33 @@ Scenario: Home Page Login
 Given User is on the Home Page
 When User logs in with a username "artur" and passwrod "12345"
 Then User accesses their profile page
-And User can access his personal details
+And User can access all personal details: "true"
 
 Scenario: Home Page Login
 Given User is on the Home Page
 When User logs in with a username "henry" and passwrod "pass"
 Then User accesses their profile page
-And User cannot access all personal details
+And User can access all personal details: "false"
+
+# Usage of Data Table for multiple parameters
+Scenario: Home Page Registration
+Given User is on the Home Page
+When User register with following details
+| jenny | jenny123 | jenny123@gmail.com | England |
+| andy | andy26 | andy@gmail.com | Scotland |
+| mark | mark88 | mark88@gmail.com | Wales |
+Then User accesses their profile page
+And User can access all personal details: "true"
+
+# Usage of Parameterisation
+Scenario Outline: Home Page Login
+Given User is on the Home Page
+When User logs in with a username <username> and password <password>
+Then User accesses their profile page
+And User can access all personal details: "true"
+
+Examples:
+| username | password |
+| john | john1 |
+| anna | anna2 |
+| kate | kate3 |
--- src/test/java/stepdefinition/StepDefinition.java
+++ src/test/java/stepdefinition/StepDefinition.java
@@ -3,34 +3,52 @@ package stepdefinition;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.junit.Cucumber;
+
+import java.util.List;
+
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class StepDefinition {
     @Given("^User is on the Home Page$")
     public void user_is_on_the_home_page() throws Throwable {
-    	System.out.println("User has navigated to the Home Page!");
+    	System.out.println("User has navigated to the Home Page");
     }
     
     @When("^User logs in with a username \"([^\"]*)\" and passwrod \"([^\"]*)\"$")
-    public void user_logs_in_with_a_username_and_passwrod(String arg1, String arg2) throws Throwable {
-    	System.out.println("User logged in with a Username and Password!");
+    public void user_logs_in_with_a_username_and_passwrod(String username, String password) throws Throwable {
+    	System.out.println("User logged in with a Username: " + username + " and Password: " + password);
     }
 
     @Then("^User accesses their profile page$")
     public void user_accesses_their_profile_page() throws Throwable {
-    	System.out.println("User accessed their Profile Page!");
+    	System.out.println("User accessed their Profile Page");
     }
 
-    @And("^User can access his personal details$")
-    public void user_can_access_his_personal_details() throws Throwable {
-    	System.out.println("User can access their Personal Details!");
+    @And("^User can access all personal details: \"([^\"]*)\"$")
+    public void user_can_access_all_personal_details(String access) throws Throwable {
+    	System.out.println("User can access all Personal Details: " + access + "\n");
     }
     
-    @Then("^User cannot access all personal details$")
-    public void user_cannot_access_all_personal_details() throws Throwable {
-    	System.out.println("User cannot access all Personal Details!");
+    @When("^User register with following details$")
+    public void user_register_with_following_details(DataTable dataTable) throws Throwable {
+    	List<List<String>> list = dataTable.raw();
+    	
+    	System.out.println("User Registers with following details:");					
+    	
+    	for (int i = 0; i < list.size(); i++) {
+			System.out.println("\nName: " + list.get(i).get(0) +
+								"\nPassword: " + list.get(i).get(1) +
+								"\nEmail: " + list.get(i).get(2) +
+								"\nNationality: " + list.get(i).get(3));
+    	}
+    }
+    
+    @When("^User logs in with a username (.+) and password (.+)$")
+    public void user_logs_in_with_following_username_and_password(String username, String password) throws Throwable {
+    	System.out.println("Username: " + username + ", Password: " + password);
     }
 }
