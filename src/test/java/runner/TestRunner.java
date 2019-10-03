e67945e155ef77266b62d7a957663b17337d9579
Artur Torres <artur.sukiennik@wipro.com>
2019-10-03 11:35:55 +0100

    json and html reporting added to Test Runner cucumber options

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features",
 				 glue = "stepdefinition",
-				 tags = "@SmokeTest, @WebTest, @MobileTest, @RegistrationTest")
+				 tags = "@SmokeTest, @WebTest, @MobileTest, @RegistrationTest",
+				 plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "junit:target/cucumber.html"})
 public class TestRunner {
 
 }
