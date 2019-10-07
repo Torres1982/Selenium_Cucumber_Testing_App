12ee8d45d722ebfb83c193e0c292bebb3b82c98c
Artur Sukiennik <artur.sukiennik@wipro.com>
2019-10-07 14:30:54 +0100

    Search Step Definition complete.

--- src/test/java/features/Search.feature
+++ src/test/java/features/Search.feature
@@ -1,14 +1,14 @@
 Feature: Search and Place Orders for Clothes
 
-@SeleniumTest
 Scenario: Search for items and validate the result
 Given User is on the eBay main page
-When User search for "shoes" items
-Then "shoes" results are displayed on the page
+When User search for "Shoes" items
+Then "Shoes" results are displayed on the page
 
+@SeleniumTest
 Scenario: Select the item and navigate to a Checkout page
 Given User is on the eBay main page
-When User search for "nike" items
-And added the items to a cart
+When User search for "Nike" items
+Then "Nike" results are displayed on the page
 And User proceeded to the Checkout page to buy the items
-Then verified selected items are displayed on the Checkout page
+Then verified selected "Nike" items are displayed on the Checkout page
--- src/test/java/stepdefinition/SearchStepDefinition.java
+++ src/test/java/stepdefinition/SearchStepDefinition.java
@@ -1,12 +1,9 @@
 package stepdefinition;
 
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
@@ -32,22 +29,19 @@ public class SearchStepDefinition {
 	
 	@Then("^\"([^\"]*)\" results are displayed on the page$")
 	public void shoes_results_are_displayed_on_page(String itemName) throws Throwable {
-		webDriver.get("https://www.ebay.ie/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=shoes&_sacat=0");
+		webDriver.get("https://www.ebay.ie/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=Nike&_sacat=0");
 		Assert.assertTrue(webDriver.findElement(By.className("vip")).getText().contains(itemName));
-	}
-
-	@When("^added the items to a cart$")
-	public void added_the_items_to_a_cart() throws Throwable {
-
 	}
 
 	@When("^User proceeded to the Checkout page to buy the items$")
 	public void user_proceeded_to_the_Checkout_page_to_buy_the_items() throws Throwable {
-
+		webDriver.findElement(By.className("vip")).click();
+		webDriver.findElement(By.cssSelector("#binBtn_btn")).click();
+		webDriver.findElement(By.cssSelector("#sbin-gxo-btn")).click();
 	}
 
-	@Then("^verified selected items are displayed on the Checkout page$")
-	public void verified_selected_items_are_displayed_on_the_Checkout_page() throws Throwable {
-
+	@Then("^verified selected \"([^\"]*)\" items are displayed on the Checkout page$")
+	public void verified_selected_items_are_displayed_on_the_Checkout_page(String itemName) throws Throwable {
+		Assert.assertTrue(webDriver.findElement(By.className("item-title")).getText().contains(itemName));
 	}
 }
